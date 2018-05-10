d738071b8689aefb5ec392abae72e25df51e698c
Darin Kadrioski <dkadrios@gmail.com>
2018-05-09 20:28:57 -0700

    Adding Stompblock mappings

--- src/getMappings.js
+++ src/getMappings.js
@@ -16,71 +16,73 @@ import F14 from './mappings/EZDrummer Latin Percussion.js'
 import F15 from './mappings/EZDrummer.js'
 import F16 from './mappings/General MIDI.js'
 import F17 from './mappings/Infinite Instruments.js'
-import F18 from './mappings/Kontakt 2.js'
-import F19 from './mappings/Ocean Way Drums.js'
-import F20 from './mappings/Pearl Redbox.js'
-import F21 from './mappings/Roland HandSonic 15.js'
-import F22 from './mappings/Roland TD-10 Perc 1.js'
-import F23 from './mappings/Roland TD-10 Perc 2.js'
-import F24 from './mappings/Roland TD-10 Perc 3.js'
-import F25 from './mappings/Roland TD-10 Perc 4.js'
-import F26 from './mappings/Roland TD-10.js'
-import F27 from './mappings/Roland TD-12 Perc 1 (Latin).js'
-import F28 from './mappings/Roland TD-12 Perc 2 (Indian).js'
-import F29 from './mappings/Roland TD-12 Perc 3 (African).js'
-import F30 from './mappings/Roland TD-12 Perc 4 (Salsa).js'
-import F31 from './mappings/Roland TD-12 Perc 5 (SFX).js'
-import F32 from './mappings/Roland TD-12 Perc 6-8 (User).js'
-import F33 from './mappings/Roland TD-12.js'
-import F34 from './mappings/Roland TD-20 Perc.js'
-import F35 from './mappings/Roland TD-20.js'
-import F36 from './mappings/Roland TD-3.js'
-import F37 from './mappings/Roland TD-30 Perc 1 (Latin).js'
-import F38 from './mappings/Roland TD-30 Perc 2 (Indian).js'
-import F39 from './mappings/Roland TD-30 Perc 3 (African).js'
-import F40 from './mappings/Roland TD-30 Perc 4 (Salsa).js'
-import F41 from './mappings/Roland TD-30 Perc 5 (SFX).js'
-import F42 from './mappings/Roland TD-30 Perc 6-8 (User).js'
-import F43 from './mappings/Roland TD-30.js'
-import F44 from './mappings/Roland TD-5.js'
-import F45 from './mappings/Roland TD-6 Perc 1.js'
-import F46 from './mappings/Roland TD-6 Perc 10 (Special).js'
-import F47 from './mappings/Roland TD-6 Perc 2.js'
-import F48 from './mappings/Roland TD-6 Perc 3 (Room).js'
-import F49 from './mappings/Roland TD-6 Perc 4 (Power).js'
-import F50 from './mappings/Roland TD-6 Perc 5 (Elec).js'
-import F51 from './mappings/Roland TD-6 Perc 6 (808).js'
-import F52 from './mappings/Roland TD-6 Perc 7 (Jazz).js'
-import F53 from './mappings/Roland TD-6 Perc 8 (Brush).js'
-import F54 from './mappings/Roland TD-6 Perc 9 (Perc).js'
-import F55 from './mappings/Roland TD-6.js'
-import F56 from './mappings/Roland TD-7.js'
-import F57 from './mappings/Roland TD-8 Perc 1.js'
-import F58 from './mappings/Roland TD-8 Perc 10 (Special).js'
-import F59 from './mappings/Roland TD-8 Perc 2.js'
-import F60 from './mappings/Roland TD-8 Perc 3 (Room).js'
-import F61 from './mappings/Roland TD-8 Perc 4 (Power).js'
-import F62 from './mappings/Roland TD-8 Perc 5 (Elec).js'
-import F63 from './mappings/Roland TD-8 Perc 6 (808).js'
-import F64 from './mappings/Roland TD-8 Perc 7 (Jazz).js'
-import F65 from './mappings/Roland TD-8 Perc 8 (Brush).js'
-import F66 from './mappings/Roland TD-8 Perc 9 (Perc).js'
-import F67 from './mappings/Roland TD-8.js'
-import F68 from './mappings/Roland TD-9 Perc.js'
-import F69 from './mappings/Roland TD-9.js'
-import F70 from './mappings/Simmons SD1000.js'
-import F71 from './mappings/Simmons SD5X.js'
-import F72 from './mappings/Simmons SD7.js'
-import F73 from './mappings/Simmons SD7PK.js'
-import F74 from './mappings/Simmons SD9K.js'
-import F75 from './mappings/Sonic Reality iMap Extended.js'
-import F76 from './mappings/Sonic Reality iMap Standard.js'
-import F77 from './mappings/Studio ProFiles.js'
-import F78 from './mappings/Superior Drummer.js'
-import F79 from './mappings/Yamaha DTX 700.js'
-import F80 from './mappings/Yamaha DTXplorer.js'
-import F81 from './mappings/Zendrum TRS 2.0.js'
-import F82 from './mappings/Zendrum TRS.js'
+import F18 from './mappings/John Emrich - Articulations 1.js'
+import F19 from './mappings/Kontakt 2.js'
+import F20 from './mappings/Ocean Way Drums.js'
+import F21 from './mappings/Pearl Redbox.js'
+import F22 from './mappings/Roland HandSonic 15.js'
+import F23 from './mappings/Roland TD-10 Perc 1.js'
+import F24 from './mappings/Roland TD-10 Perc 2.js'
+import F25 from './mappings/Roland TD-10 Perc 3.js'
+import F26 from './mappings/Roland TD-10 Perc 4.js'
+import F27 from './mappings/Roland TD-10.js'
+import F28 from './mappings/Roland TD-12 Perc 1 (Latin).js'
+import F29 from './mappings/Roland TD-12 Perc 2 (Indian).js'
+import F30 from './mappings/Roland TD-12 Perc 3 (African).js'
+import F31 from './mappings/Roland TD-12 Perc 4 (Salsa).js'
+import F32 from './mappings/Roland TD-12 Perc 5 (SFX).js'
+import F33 from './mappings/Roland TD-12 Perc 6-8 (User).js'
+import F34 from './mappings/Roland TD-12.js'
+import F35 from './mappings/Roland TD-20 Perc.js'
+import F36 from './mappings/Roland TD-20.js'
+import F37 from './mappings/Roland TD-3.js'
+import F38 from './mappings/Roland TD-30 Perc 1 (Latin).js'
+import F39 from './mappings/Roland TD-30 Perc 2 (Indian).js'
+import F40 from './mappings/Roland TD-30 Perc 3 (African).js'
+import F41 from './mappings/Roland TD-30 Perc 4 (Salsa).js'
+import F42 from './mappings/Roland TD-30 Perc 5 (SFX).js'
+import F43 from './mappings/Roland TD-30 Perc 6-8 (User).js'
+import F44 from './mappings/Roland TD-30.js'
+import F45 from './mappings/Roland TD-5.js'
+import F46 from './mappings/Roland TD-6 Perc 1.js'
+import F47 from './mappings/Roland TD-6 Perc 10 (Special).js'
+import F48 from './mappings/Roland TD-6 Perc 2.js'
+import F49 from './mappings/Roland TD-6 Perc 3 (Room).js'
+import F50 from './mappings/Roland TD-6 Perc 4 (Power).js'
+import F51 from './mappings/Roland TD-6 Perc 5 (Elec).js'
+import F52 from './mappings/Roland TD-6 Perc 6 (808).js'
+import F53 from './mappings/Roland TD-6 Perc 7 (Jazz).js'
+import F54 from './mappings/Roland TD-6 Perc 8 (Brush).js'
+import F55 from './mappings/Roland TD-6 Perc 9 (Perc).js'
+import F56 from './mappings/Roland TD-6.js'
+import F57 from './mappings/Roland TD-7.js'
+import F58 from './mappings/Roland TD-8 Perc 1.js'
+import F59 from './mappings/Roland TD-8 Perc 10 (Special).js'
+import F60 from './mappings/Roland TD-8 Perc 2.js'
+import F61 from './mappings/Roland TD-8 Perc 3 (Room).js'
+import F62 from './mappings/Roland TD-8 Perc 4 (Power).js'
+import F63 from './mappings/Roland TD-8 Perc 5 (Elec).js'
+import F64 from './mappings/Roland TD-8 Perc 6 (808).js'
+import F65 from './mappings/Roland TD-8 Perc 7 (Jazz).js'
+import F66 from './mappings/Roland TD-8 Perc 8 (Brush).js'
+import F67 from './mappings/Roland TD-8 Perc 9 (Perc).js'
+import F68 from './mappings/Roland TD-8.js'
+import F69 from './mappings/Roland TD-9 Perc.js'
+import F70 from './mappings/Roland TD-9.js'
+import F71 from './mappings/Simmons SD1000.js'
+import F72 from './mappings/Simmons SD5X.js'
+import F73 from './mappings/Simmons SD7.js'
+import F74 from './mappings/Simmons SD7PK.js'
+import F75 from './mappings/Simmons SD9K.js'
+import F76 from './mappings/Sonic Reality iMap Extended.js'
+import F77 from './mappings/Sonic Reality iMap Standard.js'
+import F78 from './mappings/Studio ProFiles.js'
+import F79 from './mappings/Superior Drummer.js'
+import F80 from './mappings/Yamaha DTX 700.js'
+import F81 from './mappings/Yamaha DTXplorer.js'
+import F82 from './mappings/Zendrum Stompblock.js'
+import F83 from './mappings/Zendrum TRS 2.0.js'
+import F84 from './mappings/Zendrum TRS.js'
 
 const getMappings = () => [
   '2box': F0,
@@ -101,70 +103,72 @@ const getMappings = () => [
   'EZDrummer': F15,
   'General MIDI': F16,
   'Infinite Instruments': F17,
-  'Kontakt 2': F18,
-  'Ocean Way Drums': F19,
-  'Pearl Redbox': F20,
-  'Roland HandSonic 15': F21,
-  'Roland TD-10 Perc 1': F22,
-  'Roland TD-10 Perc 2': F23,
-  'Roland TD-10 Perc 3': F24,
-  'Roland TD-10 Perc 4': F25,
-  'Roland TD-10': F26,
-  'Roland TD-12 Perc 1 (Latin)': F27,
-  'Roland TD-12 Perc 2 (Indian)': F28,
-  'Roland TD-12 Perc 3 (African)': F29,
-  'Roland TD-12 Perc 4 (Salsa)': F30,
-  'Roland TD-12 Perc 5 (SFX)': F31,
-  'Roland TD-12 Perc 6-8 (User)': F32,
-  'Roland TD-12': F33,
-  'Roland TD-20 Perc': F34,
-  'Roland TD-20': F35,
-  'Roland TD-3': F36,
-  'Roland TD-30 Perc 1 (Latin)': F37,
-  'Roland TD-30 Perc 2 (Indian)': F38,
-  'Roland TD-30 Perc 3 (African)': F39,
-  'Roland TD-30 Perc 4 (Salsa)': F40,
-  'Roland TD-30 Perc 5 (SFX)': F41,
-  'Roland TD-30 Perc 6-8 (User)': F42,
-  'Roland TD-30': F43,
-  'Roland TD-5': F44,
-  'Roland TD-6 Perc 1': F45,
-  'Roland TD-6 Perc 10 (Special)': F46,
-  'Roland TD-6 Perc 2': F47,
-  'Roland TD-6 Perc 3 (Room)': F48,
-  'Roland TD-6 Perc 4 (Power)': F49,
-  'Roland TD-6 Perc 5 (Elec)': F50,
-  'Roland TD-6 Perc 6 (808)': F51,
-  'Roland TD-6 Perc 7 (Jazz)': F52,
-  'Roland TD-6 Perc 8 (Brush)': F53,
-  'Roland TD-6 Perc 9 (Perc)': F54,
-  'Roland TD-6': F55,
-  'Roland TD-7': F56,
-  'Roland TD-8 Perc 1': F57,
-  'Roland TD-8 Perc 10 (Special)': F58,
-  'Roland TD-8 Perc 2': F59,
-  'Roland TD-8 Perc 3 (Room)': F60,
-  'Roland TD-8 Perc 4 (Power)': F61,
-  'Roland TD-8 Perc 5 (Elec)': F62,
-  'Roland TD-8 Perc 6 (808)': F63,
-  'Roland TD-8 Perc 7 (Jazz)': F64,
-  'Roland TD-8 Perc 8 (Brush)': F65,
-  'Roland TD-8 Perc 9 (Perc)': F66,
-  'Roland TD-8': F67,
-  'Roland TD-9 Perc': F68,
-  'Roland TD-9': F69,
-  'Simmons SD1000': F70,
-  'Simmons SD5X': F71,
-  'Simmons SD7': F72,
-  'Simmons SD7PK': F73,
-  'Simmons SD9K': F74,
-  'Sonic Reality iMap Extended': F75,
-  'Sonic Reality iMap Standard': F76,
-  'Studio ProFiles': F77,
-  'Superior Drummer': F78,
-  'Yamaha DTX 700': F79,
-  'Yamaha DTXplorer': F80,
-  'Zendrum TRS 2.0': F81,
-  'Zendrum TRS': F82,
+  'John Emrich - Articulations 1': F18,
+  'Kontakt 2': F19,
+  'Ocean Way Drums': F20,
+  'Pearl Redbox': F21,
+  'Roland HandSonic 15': F22,
+  'Roland TD-10 Perc 1': F23,
+  'Roland TD-10 Perc 2': F24,
+  'Roland TD-10 Perc 3': F25,
+  'Roland TD-10 Perc 4': F26,
+  'Roland TD-10': F27,
+  'Roland TD-12 Perc 1 (Latin)': F28,
+  'Roland TD-12 Perc 2 (Indian)': F29,
+  'Roland TD-12 Perc 3 (African)': F30,
+  'Roland TD-12 Perc 4 (Salsa)': F31,
+  'Roland TD-12 Perc 5 (SFX)': F32,
+  'Roland TD-12 Perc 6-8 (User)': F33,
+  'Roland TD-12': F34,
+  'Roland TD-20 Perc': F35,
+  'Roland TD-20': F36,
+  'Roland TD-3': F37,
+  'Roland TD-30 Perc 1 (Latin)': F38,
+  'Roland TD-30 Perc 2 (Indian)': F39,
+  'Roland TD-30 Perc 3 (African)': F40,
+  'Roland TD-30 Perc 4 (Salsa)': F41,
+  'Roland TD-30 Perc 5 (SFX)': F42,
+  'Roland TD-30 Perc 6-8 (User)': F43,
+  'Roland TD-30': F44,
+  'Roland TD-5': F45,
+  'Roland TD-6 Perc 1': F46,
+  'Roland TD-6 Perc 10 (Special)': F47,
+  'Roland TD-6 Perc 2': F48,
+  'Roland TD-6 Perc 3 (Room)': F49,
+  'Roland TD-6 Perc 4 (Power)': F50,
+  'Roland TD-6 Perc 5 (Elec)': F51,
+  'Roland TD-6 Perc 6 (808)': F52,
+  'Roland TD-6 Perc 7 (Jazz)': F53,
+  'Roland TD-6 Perc 8 (Brush)': F54,
+  'Roland TD-6 Perc 9 (Perc)': F55,
+  'Roland TD-6': F56,
+  'Roland TD-7': F57,
+  'Roland TD-8 Perc 1': F58,
+  'Roland TD-8 Perc 10 (Special)': F59,
+  'Roland TD-8 Perc 2': F60,
+  'Roland TD-8 Perc 3 (Room)': F61,
+  'Roland TD-8 Perc 4 (Power)': F62,
+  'Roland TD-8 Perc 5 (Elec)': F63,
+  'Roland TD-8 Perc 6 (808)': F64,
+  'Roland TD-8 Perc 7 (Jazz)': F65,
+  'Roland TD-8 Perc 8 (Brush)': F66,
+  'Roland TD-8 Perc 9 (Perc)': F67,
+  'Roland TD-8': F68,
+  'Roland TD-9 Perc': F69,
+  'Roland TD-9': F70,
+  'Simmons SD1000': F71,
+  'Simmons SD5X': F72,
+  'Simmons SD7': F73,
+  'Simmons SD7PK': F74,
+  'Simmons SD9K': F75,
+  'Sonic Reality iMap Extended': F76,
+  'Sonic Reality iMap Standard': F77,
+  'Studio ProFiles': F78,
+  'Superior Drummer': F79,
+  'Yamaha DTX 700': F80,
+  'Yamaha DTXplorer': F81,
+  'Zendrum Stompblock': F82,
+  'Zendrum TRS 2.0': F83,
+  'Zendrum TRS': F84,
 ]
 export default getMappings
